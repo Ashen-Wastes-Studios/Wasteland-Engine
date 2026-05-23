91458613528580b4be4cc5545ad6e42bd3e37245
rtoue <rtoue@NICKPC>
2026-05-22 21:28:43 -0400

    some minor changes

--- Wasteland/src/Wasteland/Renderer/Renderer3D.cpp
+++ Wasteland/src/Wasteland/Renderer/Renderer3D.cpp
@@ -241,7 +241,7 @@ namespace Wasteland {
         {
             if (s_Data.m_SceneInstances.empty()) return;
 
-            s_Data.SamplesPerPixel = 4;
+            s_Data.SamplesPerPixel = 8;
 
             EditorCamera editorCamera;
 
