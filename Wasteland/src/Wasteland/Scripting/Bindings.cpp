3f5a7e858ae4d576ed3afeea91378de7e8002826
rtoue <rtoue@NICKPC>
2026-05-25 15:26:37 -0400

    modified the bindings for the python scripting still crashes

--- Wasteland/src/Wasteland/Scripting/Bindings.cpp
+++ Wasteland/src/Wasteland/Scripting/Bindings.cpp
@@ -1,31 +1,59 @@
 #include "wlpch.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/operators.h>
 
 #include <Wasteland/Core/Log.h>
 #include <Wasteland/Scene/Entity.h>
 #include <Wasteland/Scene/Components.h>
+#include <Wasteland/Scene/Scene.h>
 #include "Wasteland/Core/Input.h"
 #include "Wasteland/Core/KeyCodes.h"
 #include "Wasteland/Core/MouseButtonCodes.h"
 
+#include <glm/glm.hpp>
+
 namespace py = pybind11;
+
+// Type casters for glm::vec3 to support Python tuples
+namespace pybind11::detail {
+    template <> struct type_caster<glm::vec3> {
+    public:
+        PYBIND11_TYPE_CASTER(glm::vec3, _("glm.vec3"));
+
+        bool load(handle src, bool) {
+            if (py::isinstance<py::tuple>(src)) {
+                if (PyTuple_Size(src.ptr()) != 3) return false;
+                
+                value.x = py::cast<float>(PyTuple_GetItem(src.ptr(), 0));
+                value.y = py::cast<float>(PyTuple_GetItem(src.ptr(), 1));
+                value.z = py::cast<float>(PyTuple_GetItem(src.ptr(), 2));
+                return true;
+            }
+            return false;
+        }
+
+        static handle cast(const glm::vec3& src, return_value_policy policy, handle parent) {
+            return py::cast(std::make_tuple(src.x, src.y, src.z)).release();
+        }
+    };
+}
 
 namespace Wasteland
 {
-    // Helper to bind a component to an entity
-    template <typename T>
-    void BindComponent(py::class_<Entity> &entity_class, const std::string &name)
-    {
-        entity_class.def(("Has" + name).c_str(), [](Entity &e)
-                         { return e.HasComponent<T>(); });
-        entity_class.def(("Get" + name).c_str(), [](Entity &e) -> T &
-                         { return e.GetComponent<T>(); }, py::return_value_policy::reference);
-    }
-
     PYBIND11_MODULE(Wasteland, m)
     {
-        // 1. Bind Components (Safely)
+        // 1. Bind glm::vec3 as a Python tuple-like type
+        py::class_<glm::vec3>(m, "Vec3")
+            .def(py::init<float, float, float>(), py::arg("x") = 0.0f, py::arg("y") = 0.0f, py::arg("z") = 0.0f)
+            .def_readwrite("x", &glm::vec3::x)
+            .def_readwrite("y", &glm::vec3::y)
+            .def_readwrite("z", &glm::vec3::z)
+            .def("__repr__", [](const glm::vec3& v) {
+                return "Vec3(" + std::to_string(v.x) + ", " + std::to_string(v.y) + ", " + std::to_string(v.z) + ")";
+            });
+
+        // 2. Bind Components
         if (!py::hasattr(m, "TransformComponent"))
         {
             py::class_<TransformComponent>(m, "TransformComponent")
@@ -42,24 +70,37 @@ namespace Wasteland
                 .def_readwrite("Tag", &TagComponent::Tag);
         }
 
-        // 2. Bind Entity
+        // 3. Bind Entity
         if (!py::hasattr(m, "Entity"))
         {
-            // First-time initialization
             py::class_<Entity> entity_cls(m, "Entity");
 
-            BindComponent<TransformComponent>(entity_cls, "Transform");
-            BindComponent<TagComponent>(entity_cls, "Tag");
+            // Constructor - allow Entity to be passed from C++
+            entity_cls.def(py::init<>())
+                .def(py::init<const Entity&>());
 
-            entity_cls.def("HasComponentTransform", [](Entity &e)
-                           { return e.HasComponent<TransformComponent>(); })
-                .def("GetTransform", [](Entity &e) -> TransformComponent &
-                     { return e.GetComponent<TransformComponent>(); }, py::return_value_policy::reference)
-                .def("AddTransform", [](Entity &e)
-                     { return e.AddComponent<TransformComponent>(); }, py::return_value_policy::reference);
+            // Basic entity methods
+            entity_cls.def("HasTransform", [](Entity &e) { 
+                return e.HasComponent<TransformComponent>(); 
+            })
+            .def("GetTransform", [](Entity &e) -> TransformComponent& { 
+                return e.GetComponent<TransformComponent>(); 
+            }, py::return_value_policy::reference)
+            .def("AddTransform", [](Entity &e) -> TransformComponent& { 
+                return e.AddComponent<TransformComponent>(); 
+            }, py::return_value_policy::reference)
+            .def("HasTag", [](Entity &e) { 
+                return e.HasComponent<TagComponent>(); 
+            })
+            .def("GetTag", [](Entity &e) -> TagComponent& { 
+                return e.GetComponent<TagComponent>(); 
+            }, py::return_value_policy::reference)
+            .def("AddTag", [](Entity &e, const std::string& tag) -> TagComponent& { 
+                return e.AddComponent<TagComponent>(tag); 
+            }, py::return_value_policy::reference);
         }
 
-        // 3. Bind Scene
+        // 4. Bind Scene
         if (!py::hasattr(m, "Scene"))
         {
             py::class_<Scene, Ref<Scene>>(m, "Scene")
@@ -67,10 +108,9 @@ namespace Wasteland
                 .def("DestroyEntity", &Scene::DestroyEntity);
         }
 
-        // 4. Utility functions
-        m.def("IsKeyPressed", [](const std::string &keyName)
-              {
-                  return Input::IsKeyPressed(WL_KEY_W); // Note: Keep your key mapping logic here
-              });
+        // 5. Utility functions
+        m.def("IsKeyPressed", [](int keyCode) {
+            return Input::IsKeyPressed(keyCode);
+        }, py::arg("keyCode"));
     }
 }
