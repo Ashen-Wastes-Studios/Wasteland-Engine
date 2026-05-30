be1ebe908938cfb7539319233c4ba81e31f0b07d
rtoue <rtoue@NICKPC>
2026-05-30 13:27:12 -0400

    minor changes

--- bin/Release-windows-x86_64/DemonCore-Editor/assets/scripts/Player.py
+++ bin/Release-windows-x86_64/DemonCore-Editor/assets/scripts/Player.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(__file__))
 import Wasteland
 
 class Player:
-    def __init__(self, entity=None):
+    def __init__(self, entity):
         self.entity = entity
         self.speed = 10.0
 
