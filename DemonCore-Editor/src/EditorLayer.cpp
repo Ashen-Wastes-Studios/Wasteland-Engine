4924b1579f697a7950efd73df59c27d259ab7926
rtoue <rtoue@NICKPC>
2026-07-05 01:16:03 -0400

    added fps counter

--- DemonCore-Editor/src/EditorLayer.cpp
+++ DemonCore-Editor/src/EditorLayer.cpp
@@ -275,6 +275,11 @@ namespace Wasteland
 
 			ImGui::Separator();
 
+			ImGui::Text("Application FPS: %.1f", io.Framerate);
+			ImGui::Text("Frame Time: %.3f ms", 1000.0f / io.Framerate);
+
+			ImGui::Separator();
+
 			auto stats = Wasteland::Renderer2D::GetStats();
 			ImGui::Text("Renderer2D Stats:");
 			ImGui::Text("Draw Calls: %d", stats.DrawCalls);
